62b5f45f84dad400ba929b5a899ec5124a206781
Crowdbotics <engineering@crowdbotics.com>
2023-03-10 08:41:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/connectordelete_dev_68646/MainActivity.java
@@ -0,0 +1,15 @@
+package com.connectordelete_dev_68646;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "connectordelete_dev_68646";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "connectordelete-dev-68646",
+  url: "https://connectordelete-dev-68646.botics.co",
+  api: "https://connectordelete-dev-68646.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
