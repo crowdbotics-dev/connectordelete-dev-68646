ba7ffb15d1a71e693121c49bbf83717dcb717acd
Crowdbotics <engineering@crowdbotics.com>
2023-03-10 09:33:00 +0000

    Update store.

--- store/cbconnectro/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-const cbconnectro = axios.create({
-  baseURL: "https://googl.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
