35ba1cd199d95c7f986fcf43495925e8c73447cb
Crowdbotics <engineering@crowdbotics.com>
2023-03-10 09:39:08 +0000

    Update store.

--- store/oldapp/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-const oldapp = axios.create({
-  baseURL: "https://google.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
