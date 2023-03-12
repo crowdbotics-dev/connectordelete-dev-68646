e4a23c949d8073efd6d51e71526c0b34b6e38844
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-03-13 00:17:02 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "connectordelete-dev-68646",
   "url": "https://connectordelete-dev-68646.botics.co",
   "api": "https://connectordelete-dev-68646.botics.co/api/v1",
-  "initialRoute": "Untitled1"
+  "initialRoute": "Untitled9"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
